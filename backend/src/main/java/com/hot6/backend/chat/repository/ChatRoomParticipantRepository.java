4bd482d638544cf37ee6d6cc52312cedef631a52
Ahngyuho <gyuho0897@gmail.com>
2025-04-17 17:34:01 +0900

    [Feat] 채팅방 나가기 (#19)
    채팅방 나가기 soft delete

--- backend/src/main/java/com/hot6/backend/chat/repository/ChatRoomParticipantRepository.java
+++ backend/src/main/java/com/hot6/backend/chat/repository/ChatRoomParticipantRepository.java
@@ -2,6 +2,7 @@ package com.hot6.backend.chat.repository;
 
 import com.hot6.backend.chat.model.ChatRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,10 @@ public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomPar
     // ChatRoomParticipantRepositoryCustom.java
     List<Long> findChatRoomIdsByUserId(Long userId);
     Optional<ChatRoomParticipant> findByUserIdxAndChatRoomIdx(Long userIdx, Long chatRoomIdx);
+
+    @Query("""
+SELECT p FROM ChatRoomParticipant p
+WHERE p.user.idx = :userIdx AND p.chatRoom.idx = :chatRoomIdx
+""")
+    Optional<ChatRoomParticipant> findByUserIdAndChatRoomIdSimple(Long userIdx,Long chatRoomIdx);
 }
