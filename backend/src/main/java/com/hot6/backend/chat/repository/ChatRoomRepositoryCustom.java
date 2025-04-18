cddb5d945771ddaa795b0ba398ad0f4a65b24101
Ahngyuho <gyuho0897@gmail.com>
2025-04-18 18:05:38 +0900

    [Feat] 무한 스크롤을 위한 채팅방 목록 조회 기능(#91,#69)
    slice 타입으로 변환하게 코드 작성

--- backend/src/main/java/com/hot6/backend/chat/repository/ChatRoomRepositoryCustom.java
+++ backend/src/main/java/com/hot6/backend/chat/repository/ChatRoomRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.hot6.backend.chat.repository;
 
 import com.hot6.backend.chat.model.ChatRoom;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public interface ChatRoomRepositoryCustom {
-    List<ChatRoom> findChatRoomsWithDetailsByIds(List<Long> roomIds);
+    Slice<ChatRoom> findChatRoomsWithDetailsByIds(List<Long> roomIds, Pageable pageable);
+    
 }
--- backend/src/main/java/com/hot6/backend/chat/service/ChatRoomService.java
+++ backend/src/main/java/com/hot6/backend/chat/service/ChatRoomService.java
@@ -1,17 +1,18 @@
 package com.hot6.backend.chat.service;
 
 import com.hot6.backend.chat.model.*;
-import com.hot6.backend.chat.repository.ChatMessageRepository;
 import com.hot6.backend.chat.repository.ChatRoomRepository;
 import com.hot6.backend.common.BaseResponseStatus;
 import com.hot6.backend.common.exception.BaseException;
 import com.hot6.backend.user.UserService;
 import com.hot6.backend.user.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -27,8 +28,10 @@ public class ChatRoomService {
     private final UserService userService;
     private final ChatMessageService chatMessageService;
 
-    public List<ChatDto.ChatInfo> getList() {
-        return chatRoomRepository.findAll().stream().map(ChatDto.ChatInfo::from).collect(Collectors.toList());
+    public Slice<ChatDto.ChatRoomListDto> getList(Long userIdx, Pageable pageable) {
+        Slice<ChatRoom> chatRooms = chatRoomRepository.findAllWithSlice(pageable);
+        //ChatInfo 에 정보 추가(isParticipating)
+        return chatRooms.map(room -> ChatDto.ChatRoomListDto.from(room, userIdx));
     }
 
     @Transactional
@@ -50,19 +53,22 @@ public class ChatRoomService {
         chatRoomParticipantService.save(findUser, chatRoom);
     }
 
-    public List<ChatDto.ChatInfo> getChatRoomByUserIdx(Long userIdx) {
-        List<ChatRoom> allByParticipants = chatRoomRepository.findAllByParticipants(userIdx);
-        return allByParticipants.stream().map(ChatDto.ChatInfo::from).collect(Collectors.toList());
-    }
+//    public List<ChatDto.ChatInfo> getChatRoomByUserIdx(Long userIdx) {
+//        List<ChatRoom> allByParticipants = chatRoomRepository.findAllByParticipants(userIdx);
+//        return allByParticipants.stream().map(ChatDto.ChatInfo::from).collect(Collectors.toList());
+//    }
 
     @Transactional(readOnly = true)
-    public List<ChatDto.ChatInfo> findMyChatRooms(Long userId) {
+    public Slice<ChatDto.MyChatRoomListDto> findMyChatRooms(Long userId, Pageable pageable) {
         List<Long> roomIds = chatRoomParticipantService.findChatRoomIdsByUserId(userId);
         if (roomIds.isEmpty()) {
-            return Collections.emptyList();
+            return new SliceImpl<>(Collections.emptyList(), pageable, false);
         }
 
-        return chatRoomRepository.findChatRoomsWithDetailsByIds(roomIds).stream().map(ChatDto.ChatInfo::from).collect(Collectors.toList());
+        // Slice를 리턴하는 커스텀 레포지토리 메서드 사용
+        Slice<ChatRoom> rooms = chatRoomRepository.findChatRoomsWithDetailsByIds(roomIds, pageable);
+
+        return rooms.map(ChatDto.MyChatRoomListDto::from);
     }
 
     public List<ChatDto.ChatElement> getChatMessages(Long chatRoomIdx, Long userIdx) {
@@ -70,11 +76,11 @@ public class ChatRoomService {
         return chatMessageService.findChatMessages(chatRoomParticipant);
     }
 
-    public ChatDto.ChatInfo getChatRoomInfo(Long chatRoomIdx) {
+    public ChatDto.ChatRoomDetailInfo getChatRoomInfo(Long chatRoomIdx) {
         ChatRoom chatRoom = chatRoomRepository.findWithParticipantsAndHashtagsById(chatRoomIdx)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.CHAT_ROOM_NOT_FOUND));
 
-        return ChatDto.ChatInfo.from(chatRoom);
+        return ChatDto.ChatRoomDetailInfo.from(chatRoom);
     }
 
     @Transactional
