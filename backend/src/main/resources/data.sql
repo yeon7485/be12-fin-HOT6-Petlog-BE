eef099d4fd6e52e32132752026b0ba810d9f35c1
parkdonghwi <ehdgnl3465@naver.com>
2025-04-20 16:17:45 +0900

    [Feat] 기본 게시판 카테고리는 넣기
    카테고리 기능 강화

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,3 +6,11 @@ INSERT INTO board_type (board_name, created_at, updated_at)
 VALUES
     ('free', '2025-04-14', '2025-04-14'),
     ('information', '2025-04-14', '2025-04-14');
+
+INSERT INTO category (name, color, type, description)
+VALUES
+    ('강아지', NULL, 'BOARD', '강아지 관련 게시글 카테고리'),
+    ('고양이', NULL, 'BOARD', '고양이 관련 게시글 카테고리'),
+    ('햄스터', NULL, 'BOARD', '햄스터 관련 게시글 카테고리'),
+    ('물고기', NULL, 'BOARD', '물고기 관련 게시글 카테고리'),
+    ('뱀', NULL, 'BOARD', '뱀 관련 게시글 카테고리');
