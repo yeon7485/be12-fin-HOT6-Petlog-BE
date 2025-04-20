58ee8f5faafc142fa4e7748c73679c8aaf7919ba
parkdonghwi <ehdgnl3465@naver.com>
2025-04-20 19:03:00 +0900

    [Fix] 일정 카테고리 기능 강화
     기능 강화

--- backend/src/main/java/com/hot6/backend/schedule/model/Schedule.java
+++ backend/src/main/java/com/hot6/backend/schedule/model/Schedule.java
@@ -1,6 +1,8 @@
 package com.hot6.backend.schedule.model;
 
+import com.hot6.backend.category.model.Category;
 import com.hot6.backend.common.BaseEntity;
+import com.hot6.backend.pet.model.Pet;
 import com.hot6.backend.user.model.User;
 import jakarta.persistence.*;
 import lombok.*;
@@ -23,7 +25,6 @@ public class Schedule extends BaseEntity {
     private String sMemo;
     private boolean recurring;
     private String repeatCycle;
-    private Long categoryIdx;
     private String placeId;
     private boolean isDeleted;
     private int repeatCount;
@@ -35,4 +36,12 @@ public class Schedule extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "user_idx")
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_idx")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_idx")  // DB에 생성될 외래키 이름
+    private Pet pet;
 }
