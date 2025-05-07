2d80abfed399681c0c2eca06c45ceba209bd1b87
seungho99 <shsm66@naver.com>
2025-05-07 12:26:24 +0900

    [Fix] Repository 메서드 수정
    Repository 메서드 수정

--- backend/src/main/java/com/hot6/backend/board/post/PostRepository.java
+++ backend/src/main/java/com/hot6/backend/board/post/PostRepository.java
@@ -5,6 +5,8 @@ import com.hot6.backend.board.post.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -30,13 +32,37 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             Pageable pageable
     );
 
-    Page<Post> findByBoardTypeAndTitleContainingIgnoreCaseOrBoardTypeAndUser_NicknameContainingIgnoreCase(
-            BoardType boardType1, String titleKeyword,
-            BoardType boardType2, String nicknameKeyword,
+    @Query("""
+            SELECT p FROM Post p
+            WHERE p.boardType = :boardType AND p.category.name = :categoryName
+              AND (
+                p.title LIKE %:keyword%
+                OR p.user.nickname LIKE %:keyword%
+              )
+            """)
+    Page<Post> searchByCategoryAndKeyword(
+            @Param("boardType") BoardType boardType,
+            @Param("categoryName") String categoryName,
+            @Param("keyword") String keyword,
             Pageable pageable
     );
 
     Page<Post> findByBoardTypeAndCategoryName(
             BoardType boardType, String categoryName, Pageable pageable
     );
+
+    @Query("""
+            SELECT p FROM Post p
+            WHERE p.boardType = :boardType
+              AND (
+                p.title LIKE %:keyword%
+                OR p.user.nickname LIKE %:keyword%
+              )
+            """)
+    Page<Post> searchByKeywordOnly(
+            @Param("boardType") BoardType boardType,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
+
 }
